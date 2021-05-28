975347d47d37518ae8f6dda7a02debf5a5c2bf7a
aspynlim <aspyn.lim@gmail.com>
2021-05-28 14:40:48 +0900

    22. GitHub 따라 만들기 예제 실습 (completed)

--- project/js/responsive.js
+++ project/js/responsive.js
@@ -10,10 +10,23 @@
     toggleElements();
   });
 
+  window.addEventListener('resize', function(){
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  })
+
   function toggleElements(){
     // toggle class 를 가지고 있는 요소의 개수가 바뀔 수 있으므로
     [].forEach.call($toggles, function(toggle) {
       toggle.classList.toggle('on');
     });
   }
+
+  function offElements(){
+    [].forEach.call($toggles, function(toggle) {
+      toggle.classList.remove('on');
+    });    
+  }
+
 })(window, document)
